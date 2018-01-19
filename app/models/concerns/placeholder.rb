35c49daa26f1a8b32ec1a36e3581a9f92e423dbb
Seth Norris <sethnorris1@gmail.com>
2018-01-19 10:27:05 -0700

    Fixed my misplacement of the concern and a typo

--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -1,7 +1,7 @@
-module Placehoder
+module Placeholder
   extend ActiveSupport::Concern
 
   def self.image_generator(height:, width:)
-    main_image: "http://placehold.it/#{height}X#{width}"
+    "http://placehold.it/#{height}x#{width}"
   end
 end
